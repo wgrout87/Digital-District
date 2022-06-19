8076314416c62653be997155f80a46df11f02e64
Will G <wgrout87@gmail.com>
2022-06-19 16:06:58 -0600

    added basic dashboard handlebars and route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
-const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
-  // console.log("======================");
-  // console.log(req.session);
-  // console.log("======================");
   Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
     attributes: ["title", "content", "created_at", "updated_at"],
     order: [["updated_at", "DESC"]],
     include: [
@@ -19,7 +18,7 @@ router.get("/", (req, res) => {
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
       // pass a single post object into the homepage template
-      res.render("homepage", {
+      res.render("dashboard", {
         posts,
         loggedIn: req.session.loggedIn,
       });
@@ -28,23 +27,6 @@ router.get("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-});
-
-router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
-
-  res.render("login", {
-    login: true,
-  });
-});
-
-router.get("/signup", (req, res) => {
-  res.render("login", {
-    login: false,
-  });
 });
 
 module.exports = router;
